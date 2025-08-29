bb3e1bedfa6f6723ea7592b85b7588c37de28aad
Yems <113016293+Yamal-0930744@users.noreply.github.com>
2025-08-29 21:25:55 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Yamal-Elshot-Portfolio/'   // <= replace with your repo name
 })
