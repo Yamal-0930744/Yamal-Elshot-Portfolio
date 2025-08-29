2d54341bab0bc6557bd6f94781d9f85075b60c55
Yems <113016293+Yamal-0930744@users.noreply.github.com>
2025-08-30 01:34:29 +0200

    a

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -36,7 +36,7 @@ export default function ContactSection() {
             {/* LinkedIn icon */}
             <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M4.98 3.5C4.98 4.88 3.86 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1s2.48 1.12 2.48 2.5zM.5 8h4V23h-4V8zm7.5 0h3.8v2.05h.05c.53-1 1.84-2.05 3.78-2.05C19.88 8 22 10.1 22 14.06V23h-4v-7.9c0-1.88-.03-4.3-2.62-4.3-2.62 0-3.02 2.05-3.02 4.17V23h-4V8z"/></svg>
           </a>
-          <a role="listitem" aria-label="Email" href="mailto:youremail@example.com" className="social">
+          <a role="listitem" aria-label="Email" href="mailto:yamal.elshot@hotmail.com" className="social">
             {/* Mail icon */}
             <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M20 4H4c-1.1 0-2 .9-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-1.1-.9-2-2-2Zm0 4-8 5-8-5V6l8 5 8-5v2Z"/></svg>
           </a>
